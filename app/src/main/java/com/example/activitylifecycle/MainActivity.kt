edbfd1919b84cefb898a71c4a1f378123d8f3892
mahdi-del-sh <mahdidelavarann@gmail.com>
2021-09-02 11:30:22 +0430

    Chapter one completed find view by id set on click listener Toast

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        val text = "Hello toast!"
+        val duration = Toast.LENGTH_SHORT
+
+        val myToast = Toast.makeText(this,"toast",Toast.LENGTH_SHORT)
+
+        button.setOnClickListener{
+            myToast.show()
+        }
     }
 }
