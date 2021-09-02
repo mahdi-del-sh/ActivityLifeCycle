ffb40c746b82110c18cd673fcba90748a03ac571
mahdi-del-sh <mahdidelavarann@gmail.com>
2021-09-02 19:09:55 +0430

    Chapter three started landscape layout created

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.properties.Delegates
+private const val TAG : String = "CallBacks"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button : Button
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"onCreate")
 
         button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
@@ -30,6 +33,33 @@ class MainActivity : AppCompatActivity() {
             addOne()
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy")
+    }
+
+
 
     private fun addOne(){
         number++
